e0a4a1512abfbaf607977873bffab5b77d3eb053
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-03 10:42:46 +0530

    Debugged some code...

--- src/Config/Authentication.jsx
+++ src/Config/Authentication.jsx
@@ -6,24 +6,15 @@ import { useEffect, useState } from "react";
 // import { useSelector } from "react-redux";
 
 const Authentication = () => {
-  // const { loggedInUser } = useSelector((state) => state.allUsers);
   const Navigate = useNavigate();
   const [loggedIn, setloggedIn] = useState(false);
   useEffect(() => {
-    // console.log(JSON.parse(localStorage.getItem("loggedInUser")));
     if (JSON.parse(localStorage.getItem("loggedInUser"))) {
       setloggedIn(true);
     } else {
       setloggedIn(false);
       Navigate("/login");
     }
-
-    // if (loggedInUser.length > 0) {
-    //   setloggedIn(true);
-    // } else {
-    //   setloggedIn(false);
-    //   Navigate("/login");
-    // }
   }, [Navigate]);
   return (
     loggedIn && (
